fb965024d27f6a751d20709ef7c58806596dd286
Mahad <mahad@xconn.io>
2024-03-06 20:58:46 +0500

    split parametrized tests for utils

--- tests/messages/test_util.py
+++ tests/messages/test_util.py
@@ -3,19 +3,20 @@ import pytest
 from wamp.messages import util, error
 
 
-@pytest.mark.parametrize(
-    "realm, error_message, expected_error",
-    [
-        (None, "error", "realm cannot be null for "),
-        (1, "error", "realm must be of type string for "),
-        ({"k": "v"}, "error", "realm must be of type string for "),
-    ],
-)
-def test_validate_realm_or_raise_incorrectly(realm, error_message, expected_error):
+def test_validate_realm_or_raise_with_realm_none():
+    error_message = "error"
     with pytest.raises(error.InvalidRealmError) as exc_info:
-        util.validate_realm_or_raise(realm, error_message)
+        util.validate_realm_or_raise(None, error_message)
 
-    assert str(exc_info.value) == expected_error + error_message
+    assert str(exc_info.value) == f"realm cannot be null for {error_message}"
+
+
+def test_validate_realm_or_raise_with_invalid_realm_type():
+    error_message = "error"
+    with pytest.raises(error.InvalidRealmError) as exc_info:
+        util.validate_realm_or_raise(1, error_message)
+
+    assert str(exc_info.value) == f"realm must be of type string for {error_message}"
 
 
 def test_validate_realm_or_raise_correctly():
@@ -24,19 +25,20 @@ def test_validate_realm_or_raise_correctly():
     assert result == realm
 
 
-@pytest.mark.parametrize(
-    "details, error_message, expected_error",
-    [
-        (None, "hello", "details must be of type dictionary for "),
-        (1, "hello", "details must be of type dictionary for "),
-        ({23: "v"}, "error", "invalid type for key '23' in extra details for "),
-    ],
-)
-def test_validate_details_or_raise_incorrectly(details, error_message, expected_error):
+def test_validate_details_or_raise_with_invalid_detail_type():
+    error_message = "Ping"
     with pytest.raises(error.InvalidDetailsError) as exc_info:
-        util.validate_details_or_raise(details, error_message)
+        util.validate_details_or_raise(["detail"], error_message)
 
-    assert str(exc_info.value) == expected_error + error_message
+    assert str(exc_info.value) == f"details must be of type dictionary for {error_message}"
+
+
+def test_validate_details_or_raise_with_invalid_detail_key_type():
+    error_message = "Ping"
+    with pytest.raises(error.InvalidDetailsError) as exc_info:
+        util.validate_details_or_raise({23: "v"}, error_message)
+
+    assert str(exc_info.value) == f"invalid type for key '23' in extra details for {error_message}"
 
 
 @pytest.mark.parametrize("details", [{}, {"authid": "mahad"}])
@@ -46,21 +48,24 @@ def test_validate_details_or_raise_correctly(details):
     assert result == details
 
 
-@pytest.mark.parametrize(
-    "session_id, error_message, expected_error",
-    [
-        (None, "hello", "session ID must be an integer for "),
-        (1.0, "hello", "session ID must be an integer for "),
-        ({"k": "v"}, "error", "session ID must be an integer for "),
-        (-22, "error", "invalid Session ID value for "),
-        (9007199254740993, "error", "invalid Session ID value for "),
-    ],
-)
-def test_validate_session_id_or_raise_incorrectly(session_id, error_message, expected_error):
-    with pytest.raises(error.ProtocolError) as exc_info:
-        util.validate_session_id_or_raise(session_id, error_message)
+def test_validate_session_id_or_raise_with_invalid_session_type():
+    error_message = "Pong"
+    invalid_values = [None, 1.0, {"k": "v"}]
+    for session_id in invalid_values:
+        with pytest.raises(error.ProtocolError) as exc_info:
+            util.validate_session_id_or_raise(session_id, error_message)
 
-    assert str(exc_info.value) == expected_error + error_message
+        assert str(exc_info.value) == f"session ID must be an integer for {error_message}"
+
+
+def test_validate_session_id_or_raise_with_out_of_range_session_id():
+    error_message = "Pong"
+    invalid_values = [-10, 9007199254740993]
+    for session_id in invalid_values:
+        with pytest.raises(error.ProtocolError) as exc_info:
+            util.validate_session_id_or_raise(session_id, error_message)
+
+        assert str(exc_info.value) == f"invalid Session ID value for {error_message}"
 
 
 @pytest.mark.parametrize("session_id", [1, 30, 9007199254740992])
--- tests/messages/test_welcome.py
+++ tests/messages/test_welcome.py
@@ -1,5 +1,3 @@
-from typing import Any
-
 import pytest
 
 from wamp.messages import error, util
