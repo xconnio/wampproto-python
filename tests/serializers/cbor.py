cf86c22ed2627167431343e323e4f78cf6c92111
Mahad <mahad@xconn.io>
2024-03-08 17:20:27 +0500

    Add CBOR serializer (#15)

---
+++ tests/serializers/cbor.py
@@ -0,0 +1,15 @@
+from wamp.messages.hello import Hello
+from wamp.serializers.cbor import CBORSerializer
+
+
+def test_serializer():
+    serializer = CBORSerializer()
+    hello = Hello.parse([1, "realm1", {"roles": {"caller": {}}}])
+
+    data = serializer.serialize(hello)
+    assert isinstance(data, bytes)
+
+    obj = serializer.deserialize(data)
+    assert isinstance(obj, Hello)
+    assert obj.realm == hello.realm
+    assert obj.roles == hello.roles
---
+++ wamp/serializers/cbor.py
@@ -0,0 +1,14 @@
+import cbor2
+
+from wamp.messages.message import Message
+from wamp.serializers.serializer import to_message
+from wamp.serializers.serializer import Serializer
+
+
+class CBORSerializer(Serializer):
+    def serialize(self, message: Message) -> bytes:
+        return cbor2.dumps(message.marshal())
+
+    def deserialize(self, data: bytes) -> Message:
+        wamp_message = cbor2.loads(data)
+        return to_message(wamp_message)
