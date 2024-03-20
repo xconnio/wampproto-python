7b6016c0067b72c441888993cc4cf54f05c6f881
Omer Akram <omer@thing.com>
2024-03-21 04:11:16 +0500

    implement cryptosign authenticator (client side)

---
+++ wamp/auth/cryptosign.py
@@ -0,0 +1,39 @@
+import binascii
+
+import nacl.signing
+from nacl.encoding import HexEncoder
+
+from wamp import messages
+from wamp.auth.auth import IClientAuthenticator, CLIENT_ROLES
+
+
+class CryptoSignAuthenticator(IClientAuthenticator):
+    TYPE = "cryptosign"
+
+    def __init__(self, authid: str, auth_extra: dict, private_key: str):
+        super().__init__()
+        self._authid = authid
+        self._auth_extra = auth_extra
+        self._private_key = nacl.signing.SigningKey(binascii.a2b_hex(private_key))
+
+        if "public_key" not in self._auth_extra:
+            public_key = self._private_key.verify_key.encode(HexEncoder).decode()
+            self._auth_extra["public_key"] = public_key
+
+    def details(self) -> dict:
+        return {
+            "authmethods": [CryptoSignAuthenticator.TYPE],
+            "authid": self._authid,
+            "authextra": self._auth_extra,
+            "roles": CLIENT_ROLES,
+        }
+
+    def authenticate(self, challenge: messages.Challenge) -> messages.Authenticate:
+        challenge_hex = challenge.extra.get("challenge")
+        if challenge_hex is None:
+            raise ValueError("challenge string missing in extra")
+
+        raw_challenge = binascii.a2b_hex(challenge_hex)
+        signed = self._private_key.sign(raw_challenge, HexEncoder).decode()
+
+        return messages.Authenticate(signed + challenge_hex, {})
