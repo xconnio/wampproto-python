3a14ed9a36aa205634f9c5e4d09dcf70fbbb0ef7
Omer Akram <omer@thing.com>
2024-03-21 16:02:32 +0500

    define interface for server side authenticator

--- wamp/auth/auth.py
+++ wamp/auth/auth.py
@@ -14,3 +14,49 @@ class IClientAuthenticator:
 
     def authenticate(self, challenge: messages.Challenge) -> messages.Authenticate:
         raise NotImplementedError()
+
+
+class Request:
+    def __init__(self, method: str, realm: str, authid: str, auth_extra: dict):
+        self._method = method
+        self._realm = realm
+        self._authid = authid
+        self._auth_extra = auth_extra
+
+    @property
+    def method(self) -> str:
+        return self._method
+
+    @property
+    def realm(self) -> str:
+        return self._realm
+
+    @property
+    def authid(self) -> str:
+        return self._authid
+
+    @property
+    def auth_extra(self) -> dict:
+        return self._auth_extra
+
+
+class Response:
+    def __init__(self, authid: str, authrole: str):
+        self._authid = authid
+        self._authrole = authrole
+
+    @property
+    def authid(self) -> str:
+        return self._authid
+
+    @property
+    def authrole(self) -> str:
+        return self._authrole
+
+
+class IServerAuthenticator:
+    def methods(self) -> list[str]:
+        raise NotImplementedError()
+
+    def authenticate(self, request: Request) -> Response:
+        raise NotImplementedError()
