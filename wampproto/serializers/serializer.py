1232a8f280f5c4dafd969763565c9ccdd627b79b
Mahad <mahad@xconn.io>
2024-04-02 16:59:14 +0500

    parse missing wamp messages in serializer (#43)
    * parse missing wamp messages
    
    * parse more wamp messages
    
    * rename message type to type

--- wampproto/serializers/serializer.py
+++ wampproto/serializers/serializer.py
@@ -30,5 +30,36 @@ def to_message(message: list) -> messages.Message:
             return messages.Authenticate.parse(message)
         case messages.Goodbye.TYPE:
             return messages.Goodbye.parse(message)
+        case messages.Call.TYPE:
+            return messages.Call.parse(message)
+        case messages.Invocation.TYPE:
+            return messages.Invocation.parse(message)
+        case messages.Yield.TYPE:
+            return messages.Yield.parse(message)
+        case messages.Result.TYPE:
+            return messages.Result.parse(message)
+        case messages.Register.TYPE:
+            return messages.Register.parse(message)
+        case messages.Registered.TYPE:
+            return messages.Registered.parse(message)
+        case messages.UnRegister.TYPE:
+            return messages.UnRegister.parse(message)
+        case messages.UnRegistered.TYPE:
+            return messages.UnRegistered.parse(message)
+        case messages.Subscribe.TYPE:
+            return messages.Subscribe.parse(message)
+        case messages.Subscribed.TYPE:
+            return messages.Subscribed.parse(message)
+        case messages.UnSubscribe.TYPE:
+            return messages.UnSubscribe.parse(message)
+        case messages.UnSubscribed.TYPE:
+            return messages.UnSubscribed.parse(message)
+        case messages.Publish.TYPE:
+            return messages.Publish.parse(message)
+        case messages.Published.TYPE:
+            return messages.Published.parse(message)
+        case messages.Event.TYPE:
+            return messages.Event.parse(message)
+
         case _:
             raise ValueError("unknown message type")
