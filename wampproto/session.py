d843aec5b4c67eae93af6e0746b38836f58f79f9
Omer Akram <omer@thing.com>
2024-04-02 16:00:34 +0500

    session: implement wamp unregister

--- wampproto/session.py
+++ wampproto/session.py
@@ -10,6 +10,7 @@ class WAMPSession:
         self._register_requests: dict[int, int] = {}
         self._registrations: dict[int, int] = {}
         self._invocation_requests: dict[int, int] = {}
+        self._unregister_requests: dict[int, int] = {}
         # data structures for PubSub
         self._publish_requests: dict[int, int] = {}
         self._subscribe_requests: dict[int, int] = {}
@@ -23,6 +24,10 @@ class WAMPSession:
         elif isinstance(msg, messages.Register):
             data = self._serializer.serialize(msg)
             self._register_requests[msg.request_id] = msg.request_id
+            return data
+        elif isinstance(msg, messages.UnRegister):
+            data = self._serializer.serialize(msg)
+            self._unregister_requests[msg.request_id] = msg.registration_id
             return data
         elif isinstance(msg, messages.Yield):
             if msg.request_id not in self._invocation_requests:
@@ -61,6 +66,18 @@ class WAMPSession:
                 raise ValueError("received REGISTERED for invalid request_id")
 
             self._registrations[msg.registration_id] = msg.registration_id
+
+            return msg
+        elif isinstance(msg, messages.UnRegistered):
+            try:
+                registration_id = self._unregister_requests.pop(msg.request_id)
+            except KeyError:
+                raise ValueError("received UNREGISTERED for invalid request_id")
+
+            try:
+                self._registrations.pop(registration_id)
+            except KeyError:
+                raise ValueError("received UNREGISTERED for invalid registration_id")
 
             return msg
         elif isinstance(msg, messages.Invocation):
