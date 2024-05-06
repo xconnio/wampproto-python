395d2c7c2b09db4034a96bf8f3dee72d11d24d62
Omer Akram <omer@thing.com>
2024-05-07 01:36:01 +0500

    change internal data structure to enable shared registration

--- wampproto/dealer.py
+++ wampproto/dealer.py
@@ -10,10 +10,18 @@ class PendingInvocation:
     callee_id: int
 
 
+@dataclass
+class Registration:
+    id: int
+    procedure: str
+    registrants: dict[int, int]
+    invocation_policy: str | None = None
+
+
 class Dealer:
     def __init__(self):
-        self.registrations_by_procedure: dict[str, dict[int, int]] = {}
-        self.registrations_by_session: dict[int, dict[int, str]] = {}
+        self.registrations_by_procedure: dict[str, Registration] = {}
+        self.registrations_by_session: dict[int, dict[int, Registration]] = {}
         self.pending_calls: dict[int, PendingInvocation] = {}
 
         self.id_gen = idgen.SessionScopeIDGenerator()
@@ -29,25 +37,26 @@ class Dealer:
             raise ValueError("cannot remove non-existing session")
 
         registrations = self.registrations_by_session.pop(sid)
-        for k, v in registrations.items():
-            del self.registrations_by_procedure[v][k]
-            if len(self.registrations_by_procedure[v]) == 0:
-                del self.registrations_by_procedure[v]
+        for registration_id, registration in registrations.items():
+            registration = self.registrations_by_procedure[registration.procedure]
+            if sid in registration.registrants:
+                del registration.registrants[sid]
 
-    def has_registration(self, procedure: str):
-        return len(self.registrations_by_procedure[procedure]) != 0
+            if len(registration.registrants) == 0:
+                del self.registrations_by_procedure[registration.procedure]
+
+    def has_registration(self, procedure: str) -> bool:
+        return procedure in self.registrations_by_procedure
 
     def receive_message(self, session_id: int, message: messages.Message) -> types.MessageWithRecipient:
         if isinstance(message, messages.Call):
-            registrations = self.registrations_by_procedure.get(message.uri)
-            if registrations is None or len(registrations) == 0:
+            registration = self.registrations_by_procedure.get(message.uri)
+            if registration is None:
                 err = messages.Error(message.TYPE, message.request_id, "wamp.error.no_such_procedure")
                 return types.MessageWithRecipient(err, session_id)
 
             callee_id: int = 0
-            registration: int = 0
-            for reg_id, session in registrations.items():
-                registration = reg_id
+            for session in registration.registrants.keys():
                 callee_id = session
                 break
 
@@ -55,7 +64,7 @@ class Dealer:
             self.pending_calls[request_id] = PendingInvocation(message.request_id, session_id, callee_id)
             invocation = messages.Invocation(
                 request_id=request_id,
-                registration_id=registration,
+                registration_id=registration.id,
                 args=message.args,
                 kwargs=message.kwargs,
             )
@@ -76,31 +85,36 @@ class Dealer:
             if session_id not in self.registrations_by_session:
                 raise ValueError(f"cannot register, session {session_id} doesn't exist")
 
-            if message.uri in self.registrations_by_procedure:
+            registration = self.registrations_by_procedure.get(message.uri)
+            if registration is None:
+                registration = Registration(self.id_gen.next(), message.uri, {session_id: session_id})
+                self.registrations_by_procedure[message.uri] = registration
+                self.registrations_by_session[session_id][registration.id] = registration
+            else:
+                # TODO: implement shared registrations.
                 registered = messages.Error(
                     messages.Register.TYPE, message.request_id, "wamp.error.procedure_already_exists"
                 )
                 return types.MessageWithRecipient(registered, session_id)
 
-            registration_id = self.id_gen.next()
-            self.registrations_by_session[session_id][registration_id] = message.uri
-            if message.uri not in self.registrations_by_procedure:
-                self.registrations_by_procedure[message.uri] = {}
-
-            self.registrations_by_procedure[message.uri][registration_id] = session_id
-
-            registered = messages.Registered(message.request_id, registration_id)
+            registered = messages.Registered(message.request_id, registration.id)
             return types.MessageWithRecipient(registered, session_id)
         elif isinstance(message, messages.UnRegister):
-            if session_id not in self.registrations_by_session:
+            registrations = self.registrations_by_session.get(session_id)
+            if registrations is None:
                 raise ValueError(f"cannot unregister, session {session_id} doesn't exist")
 
-            registrations = self.registrations_by_session[session_id]
-            procedure = registrations.get(message.registration_id)
-            del self.registrations_by_session[session_id][message.registration_id]
-            del self.registrations_by_procedure[procedure][message.registration_id]
-            if len(self.registrations_by_procedure[procedure]) == 0:
-                del self.registrations_by_procedure[procedure]
+            registration = registrations.get(message.registration_id)
+            try:
+                registration.registrants.pop(session_id)
+            except KeyError:
+                raise ValueError(f"cannot unregister, session {session_id} haven't registered for {registration.id}")
+
+            if len(registration.registrants) == 0:
+                del registrations[message.registration_id]
+                del self.registrations_by_procedure[registration.procedure]
+
+            self.registrations_by_session[session_id] = registrations
 
             unregistered = messages.UnRegistered(message.request_id)
             return types.MessageWithRecipient(unregistered, session_id)
