f99311eb1d8fe86bea206d365bddeaed5c54d34b
Mahad <mahad@xconn.io>
2024-05-13 19:07:55 +0500

    Add tests for dealer & broker (#77)
    * Add tests for dealer & broker
    
    * fix lint

---
+++ tests/broker_test.py
@@ -0,0 +1,135 @@
+import pytest
+
+from wampproto import messages
+from wampproto.broker import Broker
+from wampproto.types import MessageWithRecipient
+
+
+def test_add_and_remove_session():
+    broker = Broker()
+    session_id = 1
+    broker.add_session(session_id)
+
+    # Adding duplicate session should throw an exception
+    with pytest.raises(ValueError) as exc:
+        broker.add_session(session_id)
+
+    assert str(exc.value) == "cannot add session twice"
+
+    broker.remove_session(session_id)
+
+    # Removing non-existing session should throw an exception
+    with pytest.raises(ValueError) as exc:
+        broker.remove_session(3)
+
+    assert str(exc.value) == "cannot remove non-existing session"
+
+
+def test_subscribing_to_topic():
+    broker = Broker()
+    session_id = 1
+    topic_name = "io.xconn.test"
+    broker.add_session(session_id)
+
+    subscribe = messages.Subscribe(1, topic_name)
+    message_with_recipient = broker.receive_message(session_id, subscribe)
+
+    assert message_with_recipient.recipient == session_id
+    assert isinstance(message_with_recipient.message, messages.Subscribed)
+
+    # Check subscription by topic
+    has_subscription = broker.has_subscription(topic_name)
+    assert has_subscription
+
+    # Subscribe with invalid sessionID
+    with pytest.raises(ValueError) as exc:
+        broker.receive_message(3, subscribe)
+
+    assert str(exc.value) == "cannot subscribe, session 3 doesn't exist"
+
+
+def test_unsubscribing_from_topic():
+    broker = Broker()
+    topic_name = "io.xconn.test"
+    session_id = 1
+    broker.add_session(session_id)
+
+    subscribe = messages.Subscribe(1, topic_name)
+    broker.receive_message(session_id, subscribe)
+
+    unsubscribe = messages.UnSubscribe(1, 1)
+    message_with_recipient = broker.receive_message(session_id, unsubscribe)
+
+    assert message_with_recipient.recipient == 1
+    assert isinstance(message_with_recipient.message, messages.UnSubscribed)
+
+    # Check subscription by topic
+    has_subscription = broker.has_subscription(topic_name)
+    assert not has_subscription
+
+    # Unsubscribe with invalid sessionID
+    with pytest.raises(ValueError) as exc:
+        broker.receive_message(2, unsubscribe)
+
+    assert str(exc.value) == "cannot unsubscribe, session 2 doesn't exist"
+
+    # Unsubscribe with invalid subscriptionID
+    invalid_unsubscribe = messages.UnSubscribe(1, 2)
+    with pytest.raises(ValueError) as exc:
+        broker.receive_message(1, invalid_unsubscribe)
+
+    assert str(exc.value) == f"cannot unsubscribe, subscription {invalid_unsubscribe.subscription_id} doesn't exist"
+
+    # Unsubscribe with non-existing subscriptionID
+    with pytest.raises(ValueError) as exc:
+        broker.receive_message(1, invalid_unsubscribe)
+
+    assert str(exc.value) == "cannot unsubscribe, subscription 2 doesn't exist"
+
+
+def test_receive_invalid_message():
+    broker = Broker()
+    call = messages.Call(1, "io.xconn.test")
+
+    with pytest.raises(Exception) as exc:
+        broker.receive_message(1, call)
+
+    assert str(exc.value) == "message type not supported"
+
+
+def test_publishing_to_topic():
+    broker = Broker()
+    topic_name = "io.xconn.test"
+    broker.add_session(1)
+
+    subscribe = messages.Subscribe(1, topic_name)
+    broker.receive_message(1, subscribe)
+
+    publish = messages.Publish(1, topic_name, args=[1, 2, 3])
+    messages_with_recipient = broker.receive_publish(1, publish)
+
+    assert len(messages_with_recipient.recipients) == 1
+    assert isinstance(messages_with_recipient.event, messages.Event)
+    assert messages_with_recipient.ack is None
+
+    # Publish message to a topic with no subscribers
+    publish_no_subscriber = messages.Publish(2, "topic1", args=[1, 2, 3])
+    msgs = broker.receive_publish(1, publish_no_subscriber)
+
+    assert len(msgs.recipients) == 0
+    assert msgs.event is None
+    assert msgs.ack is None
+
+    # Publish with acknowledge true
+    publish_acknowledge = messages.Publish(2, topic_name, args=[1, 2, 3], options={"acknowledge": True})
+    msg_with_recipient = broker.receive_publish(1, publish_acknowledge)
+
+    assert len(msg_with_recipient.recipients) == 1
+    assert isinstance(msg_with_recipient.event, messages.Event)
+    assert isinstance(msg_with_recipient.ack, MessageWithRecipient)
+
+    # Publish message to invalid sessionID
+    with pytest.raises(Exception) as exc:
+        broker.receive_publish(5, publish)
+
+    assert str(exc.value) == "cannot publish, session 5 doesn't exist"
---
+++ tests/dealer_test.py
@@ -0,0 +1,159 @@
+import pytest
+
+from wampproto import messages
+from wampproto.dealer import Dealer
+
+
+def test_add_and_remove_session():
+    dealer = Dealer()
+    session_id = 2
+
+    dealer.add_session(session_id)
+
+    # Adding duplicate session should raise an exception
+    with pytest.raises(ValueError) as exc:
+        dealer.add_session(session_id)
+
+    assert str(exc.value) == "cannot add session twice"
+
+    dealer.remove_session(session_id)
+
+    # Removing non-existing session should raise an exception
+    with pytest.raises(ValueError) as exc:
+        dealer.remove_session(3)
+
+    assert str(exc.value) == "cannot remove non-existing session"
+
+
+def test_register_procedure():
+    dealer = Dealer()
+    session_id = 1
+    procedure_name = "io.xconn.test"
+
+    dealer.add_session(session_id)
+
+    register = messages.Register(session_id, procedure_name)
+    message_with_recipient = dealer.receive_message(session_id, register)
+
+    assert message_with_recipient.recipient == session_id
+    assert isinstance(message_with_recipient.message, messages.Registered)
+
+    # Check registration by procedure
+    has_registration = dealer.has_registration(procedure_name)
+    assert has_registration
+
+    # Register with invalid sessionID
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(2, register)
+
+    assert str(exc.value) == "cannot register, session 2 doesn't exist"
+
+    # Register again with same procedure
+    err_with_recipient = dealer.receive_message(session_id, register)
+    assert err_with_recipient.recipient == session_id
+
+    err_message: messages.Error = err_with_recipient.message
+    assert err_message.request_id == register.request_id
+    assert err_message.message_type == messages.Register.TYPE
+    assert err_message.uri == "wamp.error.procedure_already_exists"
+
+
+def test_call_procedure_and_receive_yield():
+    dealer = Dealer()
+    session_id = 1
+    procedure_name = "io.xconn.test"
+    dealer.add_session(1)
+
+    # Register a procedure
+    register = messages.Register(session_id, procedure_name)
+    dealer.receive_message(session_id, register)
+
+    call = messages.Call(session_id, procedure_name)
+    message_with_recipient = dealer.receive_message(session_id, call)
+
+    assert message_with_recipient.recipient == session_id
+    assert isinstance(message_with_recipient.message, messages.Invocation)
+
+    # Call a non-existing procedure
+    invalid_call_message = messages.Call(session_id, "invalid")
+    error_msg_with_recipient = dealer.receive_message(session_id, invalid_call_message)
+
+    assert error_msg_with_recipient.recipient == session_id
+    assert isinstance(error_msg_with_recipient.message, messages.Error)
+
+    # Process yield message correctly
+    invocation = message_with_recipient.message
+    yield_message = messages.Yield(invocation.request_id)
+    result_msg_with_recipient = dealer.receive_message(session_id, yield_message)
+
+    assert result_msg_with_recipient.recipient == session_id
+    assert isinstance(result_msg_with_recipient.message, messages.Result)
+
+    # Receive yield for non-pending invocations
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(session_id, yield_message)
+
+    assert str(exc.value) == f"no pending calls for session {session_id}"
+
+    # Receive yield with invalid sessionID
+    msg = dealer.receive_message(session_id, call).message
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(3, messages.Yield(msg.request_id))
+
+    assert str(exc.value) == "received unexpected yield from session=3"
+
+
+def test_unregister_procedure_not_registered():
+    dealer = Dealer()
+    session_id = 1
+    dealer.add_session(session_id)
+
+    unregister = messages.UnRegister(session_id, 1)
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(session_id, unregister)
+
+    assert str(exc.value) == f"cannot unregister, session {session_id} haven't registered for 1"
+
+
+def test_unregister_procedure():
+    dealer = Dealer()
+    session_id = 1
+    procedure_name = "io.xconn.test"
+    dealer.add_session(session_id)
+
+    # Register a procedure
+    register = messages.Register(session_id, procedure_name)
+    dealer.receive_message(session_id, register)
+
+    unregister = messages.UnRegister(session_id, 1)
+    messages_with_recipient = dealer.receive_message(session_id, unregister)
+    assert messages_with_recipient.recipient == session_id
+    assert isinstance(messages_with_recipient.message, messages.UnRegistered)
+
+    # Check registration by procedure
+    has_registration = dealer.has_registration(procedure_name)
+    assert not has_registration
+
+    # Unregister with invalid sessionID
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(2, unregister)
+
+    assert str(exc.value) == "cannot unregister, session 2 doesn't exist"
+
+    # Unregister with invalid registrationID
+    invalid_unregister = messages.UnRegister(session_id, 3)
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(session_id, invalid_unregister)
+
+    assert str(exc.value) == f"cannot unregister, session {session_id} haven't registered for 3"
+
+
+def test_receive_invalid_message():
+    dealer = Dealer()
+    session_id = 1
+    procedure_name = "io.xconn.test"
+    subscribe = messages.Subscribe(session_id, procedure_name)
+    with pytest.raises(ValueError) as exc:
+        dealer.receive_message(session_id, subscribe)
+
+    assert str(exc.value) == "message type not supported"
