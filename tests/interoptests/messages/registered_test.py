20761273d19e3fc7f066531a1a294e9fb16b8c3b
Muzzammil Shahid <muzzammil@xconn.io>
2024-07-04 00:24:56 +0500

    Add interoperability tests for Registered message

---
+++ tests/interoptests/messages/registered_test.py
@@ -0,0 +1,50 @@
+import pytest
+
+from tests.interoptests.helpers import run_command
+from wampproto import messages
+from wampproto.serializers import JSONSerializer, CBORSerializer, MsgPackSerializer
+
+
+def is_equal(msg1: messages.Registered, msg2: messages.Registered) -> bool:
+    return msg1.request_id == msg2.request_id and msg1.registration_id == msg2.registration_id
+
+
+@pytest.mark.asyncio
+async def test_json_serializer():
+    msg = messages.Registered(messages.RegisteredFields(1, 1))
+    command = f"wampproto message registered {msg.request_id} {msg.registration_id} --serializer json"
+
+    output = await run_command(command)
+
+    serializer = JSONSerializer()
+    message = serializer.deserialize(output)
+
+    assert is_equal(msg, message)
+
+
+@pytest.mark.asyncio
+async def test_cbor_serializer():
+    msg = messages.Registered(messages.RegisteredFields(1, 1))
+    command = f"wampproto message registered {msg.request_id} {msg.registration_id} --serializer cbor --output hex"
+
+    output = await run_command(command)
+    output_bytes = bytes.fromhex(output)
+
+    serializer = CBORSerializer()
+    message = serializer.deserialize(output_bytes)
+
+    assert is_equal(msg, message)
+
+
+@pytest.mark.asyncio
+async def test_msgpack_serializer():
+    msg = messages.Registered(messages.RegisteredFields(1, 1))
+    command = f"wampproto message registered {msg.request_id} {msg.registration_id} --serializer msgpack --output hex"
+
+    output = await run_command(command)
+    output_bytes = bytes.fromhex(output)
+
+    serializer = MsgPackSerializer()
+    message = serializer.deserialize(output_bytes)
+
+    assert is_equal(msg, message)
