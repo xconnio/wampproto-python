684737475a6dbc6fe0d5a0594fe87c3fc0214b45
Muzzammil Shahid <muzzammil@xconn.io>
2024-07-04 16:41:51 +0500

    Add interoperability tests for Cancel message

---
+++ tests/interoptests/messages/cancel_test.py
@@ -0,0 +1,63 @@
+import pytest
+
+from tests.interoptests.helpers import run_command
+from wampproto import messages
+from wampproto.serializers import JSONSerializer, CBORSerializer, MsgPackSerializer
+
+
+def is_equal(msg1: messages.Cancel, msg2: messages.Cancel) -> bool:
+    return msg1.request_id == msg2.request_id and msg1.options == msg2.options
+
+
+@pytest.mark.asyncio
+async def test_json_serializer():
+    msg = messages.Cancel(messages.CancelFields(1))
+    command = f"wampproto message cancel {msg.request_id} --serializer json"
+
+    output = await run_command(command)
+
+    serializer = JSONSerializer()
+    message = serializer.deserialize(output)
+
+    assert is_equal(msg, message)
+
+
+@pytest.mark.asyncio
+async def test_cbor_serializer():
+    msg = messages.Cancel(messages.CancelFields(1))
+    command = f"wampproto message cancel {msg.request_id} --serializer cbor --output hex"
+
+    output = await run_command(command)
+    output_bytes = bytes.fromhex(output)
+
+    serializer = CBORSerializer()
+    message = serializer.deserialize(output_bytes)
+
+    assert is_equal(msg, message)
+
+
+@pytest.mark.asyncio
+async def test_msgpack_serializer():
+    msg = messages.Cancel(messages.CancelFields(1))
+    command = f"wampproto message cancel {msg.request_id} --serializer msgpack --output hex"
+
+    output = await run_command(command)
+    output_bytes = bytes.fromhex(output)
+
+    serializer = MsgPackSerializer()
+    message = serializer.deserialize(output_bytes)
+
+    assert is_equal(msg, message)
+
+
+@pytest.mark.asyncio
+async def test_with_options():
+    msg = messages.Cancel(messages.CancelFields(1, options={"a": "b"}))
+    command = f"wampproto message cancel {msg.request_id} -o a=b --serializer json"
+
+    output = await run_command(command)
+
+    serializer = JSONSerializer()
+    message = serializer.deserialize(output)
+
+    assert is_equal(msg, message)
