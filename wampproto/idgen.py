89f01e432c2cf024b570dd777de1766af95d935c
Mahad <mahad@xconn.io>
2025-10-22 20:02:00 +0500

    make SessionScopeIDGenerator thread-safe (#152)

--- wampproto/idgen.py
+++ wampproto/idgen.py
@@ -1,4 +1,5 @@
 import random
+import threading
 
 ID_MAX = 1 << 53
 
@@ -8,13 +9,23 @@ def generate_session_id():
 
 
 class SessionScopeIDGenerator:
-    def __init__(self):
+    def __init__(self, use_lock: bool = False):
         super().__init__()
         self.id: int = 0
+        self._use_lock: bool = use_lock
+        self._lock = threading.Lock()
 
-    def next(self):
-        if self.id == ID_MAX:
+    def _next(self) -> int:
+        if self.id >= ID_MAX:
             self.id = 0
 
         self.id += 1
         return self.id
+
+
+    def next(self) -> int:
+        if self._use_lock:
+            with self._lock:
+                return self._next()
+        else:
+            return self._next()
